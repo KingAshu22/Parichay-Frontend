09960e232bbc5accf26edc660f60c4c9df90bcd5
KingAshu22 <ashishprasad9833@gmail.com>
2024-03-17 00:43:48 +0530

    updated details

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,29 +1,34 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
 import { Truck, ShieldCheck, Wallet, Phone, Box } from "lucide-react";
 
-const Container = ({ children }) => (
+interface DetailItemProps {
+  icon: ReactNode;
+  text: string;
+}
+
+const Container = ({ children }: { children: ReactNode }) => (
   <div className="flex flex-wrap justify-center items-center gap-10 md:gap-8 px-8 py-10 max-w-screen-xl mx-auto md:py-20">
     {children}
   </div>
 );
 
-const Item = ({ children }) => (
+const Item = ({ children }: { children: ReactNode }) => (
   <div className="flex flex-col items-center text-center">{children}</div>
 );
 
-const IconContainer = ({ children }) => (
+const IconContainer = ({ children }: { children: ReactNode }) => (
   <div className="flex items-center justify-center w-16 h-16 md:w-20 md:h-20 border-2 border-black rounded-full bg-gray-900 text-white">
     {children}
   </div>
 );
 
-const Text = ({ children }) => (
+const Text = ({ children }: { children: ReactNode }) => (
   <div className="text-xs md:text-sm max-w-[80px]">{children}</div>
 );
 
-const DetailItem = ({ icon, text }) => (
+const DetailItem = ({ icon, text }: DetailItemProps) => (
   <Item>
     <IconContainer>{icon}</IconContainer>
     <Text>{text}</Text>
