1090cce7e0aa5d91492b6c029ca585858a796e8b
KingAshu22 <ashishprasad9833@gmail.com>
2024-03-17 01:48:07 +0530

    added header in bottom nav

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -3,12 +3,24 @@
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import useCart from "@/lib/hooks/useCart";
-import { Heart, Home, ShoppingBag, Box, Search } from "lucide-react";
+import {
+  Heart,
+  Home,
+  ShoppingBag,
+  Box,
+  Search,
+  CircleUserRound,
+} from "lucide-react";
+import { useState } from "react";
+import { UserButton, useUser } from "@clerk/nextjs";
 
 export default function BottomNav() {
   const pathname = usePathname();
   const router = useRouter();
   const cart = useCart();
+  const { user } = useUser();
+
+  const [query, setQuery] = useState("");
 
   const navItems = [
     { path: "/", icon: <Home />, label: "Home" },
@@ -31,23 +43,59 @@ export default function BottomNav() {
   ];
 
   return (
-    <div className="bg-gray-900 fixed bottom-0 left-0 right-0">
-      <nav className="container mx-auto pl-10 pr-10 py-2">
-        <div className="flex justify-between">
-          {navItems.map(({ path, icon, label }) => (
-            <Link
-              href={path}
-              key={path}
-              className={`flex flex-col items-center justify-center text-white py-2 ${
-                pathname === path ? "bg-red-500 px-4 rounded-lg" : ""
-              }`}
+    <>
+      <div className="bg-gray-900 fixed bottom-0 left-0 right-0">
+        <nav className="container mx-auto pl-10 pr-10 py-2">
+          <div className="flex justify-between">
+            {navItems.map(({ path, icon, label }) => (
+              <Link
+                href={path}
+                key={path}
+                className={`flex flex-col items-center justify-center text-white py-2 ${
+                  pathname === path ? "bg-red-500 px-4 rounded-lg" : ""
+                }`}
+              >
+                {icon}
+                <span className="text-sm">{label}</span>
+              </Link>
+            ))}
+          </div>
+        </nav>
+      </div>
+      <header className="bg-gray-800 sticky top-0 z-50">
+        <div className="flex justify-between items-center py-4 px-8">
+          <Link
+            href="/"
+            className="text-white text-lg font-bold -ml-5"
+            passHref
+          >
+            Parichay Designer
+          </Link>
+          <div className="flex gap-3 border border-grey-2 px-3 py-1 items-center rounded-lg">
+            <input
+              className="outline-none max-sm:max-w-[120px] bg-transparent"
+              placeholder="Search..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button
+              disabled={query === ""}
+              onClick={() => router.push(`/search/${query}`)}
             >
-              {icon}
-              <span className="text-sm">{label}</span>
-            </Link>
-          ))}
+              <Search className="text-white cursor-pointer h-4 w-4 hover:text-red-1" />
+            </button>
+          </div>
+          <div className="flex items-center">
+            {user ? (
+              <UserButton afterSignOutUrl="/sign-in" />
+            ) : (
+              <Link className="ml-10" href="/sign-in">
+                <CircleUserRound />
+              </Link>
+            )}
+          </div>
         </div>
-      </nav>
-    </div>
+      </header>
+    </>
   );
 }
